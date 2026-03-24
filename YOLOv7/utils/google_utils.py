2dcbc2a2eb183dc2e398ff4d80f359ba9679c7be
JeevarajS2910 <jeevarajs2006@gmail.com>
2026-03-24 23:29:20 +0530

    YOLOv7 training update11

--- YOLOv7/utils/google_utils.py
+++ YOLOv7/utils/google_utils.py
@@ -25,10 +25,13 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
             response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
             assets = [x['name'] for x in response['assets']]  # release assets
             tag = response['tag_name']  # i.e. 'v1.0'
-        except:  # fallback plan
+        except Exception:  # fallback plan
             assets = ['yolov7.pt', 'yolov7-tiny.pt', 'yolov7x.pt', 'yolov7-d6.pt', 'yolov7-e6.pt', 
                       'yolov7-e6e.pt', 'yolov7-w6.pt']
-            tag = subprocess.check_output('git tag', shell=True).decode().split()[-1]
+            try:
+                tag = subprocess.check_output('git tag', shell=True, stderr=subprocess.STDOUT).decode().split()[-1]
+            except Exception:
+                tag = 'v0.1'  # YOLOv7 default release tag
 
         name = file.name
         if name in assets:
